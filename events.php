3fb74577071f5f422c2992b51616887d36a09b1f
hajertamas <76472742+hajertamas@users.noreply.github.com>
2021-02-02 20:15:29 +0100

    Added front end implementation example

--- events.php
+++ events.php
@@ -12,7 +12,7 @@ $controller = new ExampleController();
 $server = new SSEServer($controller, true);
 
 //Every $cycleTime seconds the server will do a cycle (default is 1 seconds)
-$cycleTime = 3;
+$cycleTime = 1;
 $server->setCycleTime($cycleTime);
 
 //In every $pingInterval (2nd) cycle a ping event will be sent containing current time (milliseconds since epoch as int) (default is every cycle (1))
