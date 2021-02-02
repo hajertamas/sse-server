9c3e2f940771ce1b9b0d13af5dc12b0bbfd89e93
hajertamas <76472742+hajertamas@users.noreply.github.com>
2021-02-02 19:56:50 +0100

    Added documentation

--- SSEServer/Event.php
+++ SSEServer/Event.php
@@ -1,20 +1,42 @@
 <?php 
 namespace SSEServer;
 
+/**
+ * Default EventInterface implementation.
+ * Sends event data as json
+ */
 class Event implements \SSEServer\EventInterface{
 
     private $type;
     private $data;
 
+    /**
+     * @param String $eventType The type of the event. This can be anything.
+     * @param Array $data Array of data to be sent as JSON output.
+     */
     function __construct(String $eventType, Array $data){
         $this->type = $eventType;
         $this->data = $data;
     }
+
+    /**
+     * Sends the event as output.
+     */
     public function send(): Void{
+        //Send event type
         echo "event: " . $this->type . "\n";
+
+        //Send data
         echo "data: " . json_encode($this->data, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+
+        //Indicate event end
         echo "\n\n";
     }
+
+    /**
+     * Returns the event type
+     * @return String
+     */
     public function getType(): String{
         return $this->type;
     }
--- SSEServer/Events.php
+++ SSEServer/Events.php
@@ -5,40 +5,72 @@ use SSEServer\EventInterface;
 
 class Events{
 
+    /**
+     * An array which is used to store EventInterface objects
+     * @var Array
+     */
     private $events = [];
 
+    /**
+     * @param Array $eventsArray Array containing EventInterface objects.
+     */
     function __construct(Array $eventsArray = null){
+        
+        //Check if arg is null
         if(!is_null($eventsArray)){
+
+            //Iterate through input array
             foreach($eventsArray as $event){
+
                 try{
+
+                    //Check if array element implements EventInterface
                     if($event instanceof EventInterface){
+
+                        //Store the event
                         array_push($this->events, $event);
+
+                        //Skip to next iteration
                         continue;
                     }
+
+                    //Throw exception
                     throw new \Exception("Object is not an instance of EventInterface");
                     
                 }catch(\Throwable $e){
-                    $debugEvent = new Event("debug", 
-                        Array(
-                            "error" => $e->getMessage()
-                        )
-                    );
+
+                    //Create new debug event
+                    $debugEvent = new Event("debug", Array("error" => $e->getMessage()));
+
+                    //Store debug event
                     array_push($this->events, $debugEvent);
                 }
             }
         }
     }
 
+    /**
+     * Pushes an event into the object.
+     * @param EventInterface $event Event to be added to the collection
+     */
     public function push(EventInterface $event): Void{
         array_push($this->events, $event);
     }
 
+    /**
+     * Returns a clone of this object & erases all stored events
+     * @return Events
+     */
     public function flush(): Events{
         $self = clone $this;
         $this->events = [];
         return $self;
     }
-    
+
+    /**
+     * Returns all events stored
+     * @return Array
+     */
     public function getEvents(): Array{
         return $this->events;
     }
